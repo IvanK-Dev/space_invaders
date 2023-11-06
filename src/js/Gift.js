51410088580844592b2b450a2c48b874185cad1d
IvanK-Dev <zak98k@ukr.net>
2023-11-06 21:03:21 +0200

    add jsDoc to  Gift.js

--- src/js/Gift.js
+++ src/js/Gift.js
@@ -1,6 +1,19 @@
 import GameObject from './GameObject.js';
 
+/**
+ * Представляет подарок, который падает из врага при его уничтожении.
+ */
 export class Gift extends GameObject {
+  /**
+   * Создает экземпляр подарка.
+   * @constructor
+   * @param {number} x - Позиция по горизонтали.
+   * @param {number} y - Позиция по вертикали.
+   * @param {number} width - Ширина подарка.
+   * @param {number} height - Высота подарка.
+   * @param {HTMLElement} container - Родительский контейнер подарка.
+   * @param {string} giftContent - Содержимое подарка.
+   */
   constructor(x, y, width, height, container, giftContent) {
     super(x, y, width, height, container);
     this.giftContent = giftContent;
@@ -9,30 +22,41 @@ export class Gift extends GameObject {
     this.createGift();
   }
 
+  /**
+   * Создает DOM-элемент подарка и добавляет его в контейнер.
+   */
   createGift = () => {
     this.createElement();
     this.element.classList.add('gift');
 
-    this.element.innerHTML =`<p>${ this.giftContent}</p>`; // Устанавливаем тип подарка
+    this.element.innerHTML = `<p>${this.giftContent}</p>`; // Устанавливаем тип подарка
     this.element.style.backgroundColor = 'red';
 
     this.updatePosition();
-    this.updateGift()
+    this.updateGift();
   };
 
+  /**
+   * Двигает подарок вниз по экрану.
+   */
   move = () => {
     this.y += this.fallingSpeed;
     this.updatePosition();
   };
 
-  updateGift=()=>{
+  /**
+   * Обновляет позицию подарка на экране.
+   */
+  updateGift = () => {
     this.move();
     this.animationFrameId = requestAnimationFrame(this.updateGift);
-  }
-  
-  removeGift=()=>{
+  };
+
+  /**
+   * Удаляет подарок.
+   */
+  removeGift = () => {
     this.removeElemnt();
     cancelAnimationFrame(this.animationFrameId);
-
-  }
+  };
 }
